7d83dcc4cb87b784682ed06c42b09b0dfc143ad7
S1mpleKnight <become.adult77@gmail.com>
2021-10-25 11:29:39 +0300

    new: create DAOFactory & ApplianceDAO

---
+++ src/main/java/by/bsuir/poit/wt/dao/ApplianceDAO.java
@@ -0,0 +1,8 @@
+package by.bsuir.poit.wt.dao;
+
+import by.bsuir.poit.wt.entity.Appliance;
+import by.bsuir.poit.wt.entity.criteria.Criteria;
+
+public interface ApplianceDAO {
+    Appliance find(Criteria criteria);
+}
